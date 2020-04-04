e84ccfba1cb2a7e36b5985dcdb23d36cdc40f314
nikakrash <nikakrash@ya.ru>
2020-04-04 21:47:45 +0500

    добавлен новый функционал

--- script.js
+++ script.js
@@ -8,3 +8,23 @@ const selectedLevel = (item) => {
 
 level.forEach((item) => item.addEventListener("click", selectedLevel));
 
+const startButton = document.getElementById('start-button');
+
+
+function clearField() {
+	document.querySelector('.container').classList.add('invisible');
+	document.querySelector('.game-field').classList.add('game-field__wrapper');
+}
+
+function createCard() {
+	const card = document.createElement('div');
+	card.classList.add('card__front');
+	document.querySelector('.cards').append(card);
+}
+
+startButton.onclick = () => {
+	clearField();
+	for (var i = 1; i <= 3; i++) {
+		createCard();
+	}
+}
