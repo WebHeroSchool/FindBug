6ef07b6e1af65a4fa2ca10b7cab7d194f0c0ffb6
Kseniya Molkova <52568987+Costyanica@users.noreply.github.com>
2020-04-10 19:19:49 +0300

    add function getLevel

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
 const body = document.body;
 const level = document.querySelectorAll('.menu-item');
 
-const selectedLevel = (item) => {
+const selectLevel = (item) => {
     level.forEach((item) => item.classList.remove("selected"));
     item.target.classList.add("selected");
 };
 
-level.forEach((item) => item.addEventListener("click", selectedLevel));
+level.forEach((item) => item.addEventListener("click", selectLevel));
 
 const startButton = document.getElementById('start-button');
+
+function getLevel() {
+	const selectedLevel = document.querySelector(".selected");
+	let id = selectedLevel.id;
+	let numberOfCards;
+	switch (id) {
+		case "simple":
+			numberOfCards = 3;
+			break;
+		case "middle":
+			numberOfCards = 6;
+			break;
+		default:
+			numberOfCards = 9;
+	}
+	console.log(numberOfCards);
+	return numberOfCards;
+}
+
 
 
 function clearField() {
@@ -22,9 +41,13 @@ function createCard() {
 	document.querySelector('.cards').append(card);
 }
 
-startButton.onclick = () => {
+function startGame() {
 	clearField();
-	for (var i = 1; i <= 3; i++) {
+	let numberOfCards = getLevel();
+	for (var i = 1; i <= numberOfCards; i++) {
 		createCard();
 	}
+
 }
+
+startButton.addEventListener("click", startGame);
